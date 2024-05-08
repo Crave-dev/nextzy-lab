bc81a7d99e30fe53fa10993751b2db0e8cc10a24
Crave <bersorgertv@gmail.com>
2024-05-08 17:02:28 +0700

    fix: db schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,30 +1,30 @@
 import { relations } from "drizzle-orm";
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, uuid } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable('users', {
-  id: serial('id').primaryKey(),
+  id: uuid('id').primaryKey(),
   username: text('username').notNull().unique(),
   password: text('password').notNull(),
   salt: text('salt').notNull().unique()
 })
 
 export const studioTable = pgTable('studios', {
-  id: serial('id').primaryKey(),
+  id: uuid('id').primaryKey(),
   name: text('name').notNull(),
   website: text('website'),
 })
 
 export const chapterTable = pgTable('chapters', {
-  id: serial('id').primaryKey(),
+  id: uuid('id').primaryKey(),
   name: text('name').notNull(),
-  animeId: integer('anime_id')
+  animeId: text('anime_id')
 })
 
 export const animeTable = pgTable('animes', {
-  id: serial('id').primaryKey(),
+  id: uuid('id').primaryKey(),
   name: text('name').notNull(),
   year: integer('year').notNull(),
-  studioId: integer('studio_id')
+  studioId: text('studio_id')
 })
 
 export const chapterRelation = relations(chapterTable, ({ one }) => ({
